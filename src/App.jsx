6d24c27a0369d031a7d4300e3f8af4db7c62f302
kuldeep407 <kuldeepchahar426@gmail.com>
2025-03-02 00:05:55 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
 import FolderList from "./Pages/FolderList";
 
 function App() {
